0b11e2516fb7944d030d8bab1cf0bbf75dfbcecd
Kostas-Papakostas <papakostask@hotmail.com>
2015-03-18 20:20:35 +0200

    first attempt

--- lab03/hex2i.asm
+++ lab03/hex2i.asm
@@ -14,15 +14,34 @@ inmsg:
 main:   
         # Get address of input string
         la   $a0, inmsg
-
-
+	lw  $s0, 0($a0)
+	
         #################################
-        # Write your program here
-        # The following assumes the result is in $s0
+					        # Write your program here
+        					# The following assumes the result is in $s0
+       	#lw  $s0, 0($a0)
+       	#li   $t0, 8
+       	li   $t0, 7    	#7 ASCII characters
+loop:
+	srl  $t1, $s0, 24
+        slti $t2, $t1,   57
+        beq  $t2, $zero, over57
+        j    save_i
+over57:
+        subi $t1, $t1, 55
+        
+save_i:
+	sb   $t1, 0($a0)
+        addi $a0, $a0, 1
+        
+        sll  $s0, $s0, 8   # Drop current leftmost digit
+        addi $t0, $t0, -1  # Update loop counter
+        bne  $t0, $0, loop # Still not 0?, go to loop
+        
         #################################
 
         la   $a1, number # Address where the result should go to
-		sw   $s0, 0($a1)
+	sw   $s0, 0($a1)
 
         # end the program
         li   $v0, 10
