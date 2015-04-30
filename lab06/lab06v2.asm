60a0e54467a4cfb4c723eebebbfec00568a5f5aa
Kostas-Papakostas <papakostask@hotmail.com>
2015-05-01 01:18:16 +0300

    lab06

--- lab06/lab06v2.asm
+++ lab06/lab06v2.asm
@@ -9,16 +9,16 @@ number:
 main:   
 	add $t1, $zero, $zero
 	lw $t0,268500992($zero)
-	addi $t1,$t0,5
+	add $t1,$t0,$t0
 	sub $t3,$t1,$t0
-	andi $t5,$t3,100
-	ori $t6,$t3,1000
-	slti $t4,$t3,4
-	beq $t4,$zero,label1
+	and $t5,$t3,$t0
+	or $t6,$t3,$t1
+
 	
-	
+	beq $zero,$zero,label1
 label1:
 	j label2
 	
 label2:
-	addi $t4,$t3,9
+	sw $t0,($s0)
+	beq $t4,$zero,label2
--- lab06/maindec.v
+++ lab06/maindec.v
@@ -23,6 +23,10 @@ module maindec(input  [5:0] op,
       6'b101011: controls = 9'b001010000; //SW
       6'b000100: controls = 9'b000100001; //BEQ
       6'b001000: controls = 9'b101000000; //ADDI
+      6'b001000: controls = 12'b10100000; // ADDI
+      6'b001010: controls = 12'b10100000; // SLTI
+      6'b001101: controls = 12'b10100001; // ORI
+
       6'b000010: controls = 9'b000000100; //J
       default:   controls = 9'bxxxxxxxxx; //???
     endcase
